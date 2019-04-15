90fe322270fbf3f36b3842bc10b4cd8381c64aa1
Nicolas <marty.nicolas11@laposte.net>
2019-04-15 13:38:25 +0200

    Serveur Tomcat en place

---
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -0,0 +1,23 @@
+package dev.sgp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListerCollaborateursController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3155850309813211995L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+	ServletException, IOException {
+	resp.getWriter().write("Hello ListerCollaborateursController");
+	}
+	
+}
